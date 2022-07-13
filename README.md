c78ee93825c4e94995fdbcc36eaab190b2051f2c
Maksym Tkachuk <s2m4f6@gmail.com>
2022-07-14 02:55:14 +0300

    Updated readme, moved build->pybuild

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # pybuild
+
+Build tool for C++ written in python
+
+## How to run
+To build included example C++ project run `./example.py`  
+To run test run `./example.py test`  
+To run cppcheck run `./example.py cppcheck`  
+
+## Example pybuild file
+Simplest pybuild file could look like this:  
+```python
+#!/usr/bin/env python3
+
+from pybuild.config import format as cf
+import pybuild
+
+@build.task()
+def app(ctx):
+    pybuild.cpp.compile(cf('{topdir}/example/src/app.cc'))
+    pybuild.cpp.link_exe(
+       files=[cf('{build_dir}/{profile}/obj/app.o')],
+       output='app'
+    )
+
+pybuild.cli.run('app') # 'app' is default task name
+```
+
+Main executable units of pybuild are called tasks.  
+To create a task define a function and annotate it with `@pybuild.task()`:  
+```python
+@pybuild.task()
+def app(ctx):
+    ...
+```
+
+Tasks can have substasks. Subtasks are annotated with `@pybuild.subtask('parent task name')`.
+
+```python
+
+@pybuild.task()
+def lib(ctx):
+    ctx.run_subtasks()
+    pybuild.cpp.create_static_lib(
+        files=pybuild.cpp.get_objs([
+            cf('{build_dir}/{profile}/obj/utils'),
+            cf('{build_dir}/{profile}/obj/core')
+        ]),
+        output='libexample.a'
+    )
+
+@pybuild.subtask('libff')
+def core(ctx):
+    pybuild.cpp.compile_batch(pybuild.utils.get_files(cf('{topdir}/src/core'), r'.+\.cc'), 'core')
+
+@pybuild.subtask('libff')
+def utils(ctx):
+    pybuild.cpp.compile_batch(pybuild.utils.get_files(cf('{topdir}/src/utils'), r'.+\.cc'), 'utils')
+
+```
+
+Also tasks can depend on each other. Dependencies are passed as a list of task names to `build.task` annotation:  
+```python
+
+@pybuild.task()
+def install_headers(ctx):
+    ...
+
+@pybuild.task()
+def dependencies(ctx):
+    ctx.run_subtasks()
+
+@pybuild.task(['install_headers', 'dependencies'])
+def lib(ctx):
+    ...
+
+```
--- example.py
+++ example.py
@@ -1,44 +1,44 @@
 #!/usr/bin/env python3
 
-from build.config import format as cf
-import build, os, shutil
+from pybuild.config import format as cf
+import pybuild, os, shutil
 
-@build.config.feature_handler
+@pybuild.config.feature_handler
 def features(profile, feature_list):
-    if 'DEBUG' in feature_list: build.config.get('cpp', 'cxxflags').append('-D_DEBUG')
+    if 'DEBUG' in feature_list: pybuild.config.get('cpp', 'cxxflags').append('-D_DEBUG')
  
-@build.task()
+@pybuild.task()
 def install_headers(ctx):
     src, dst = cf('{topdir}/example/include'), cf('{build_dir}/{profile}/include')
     if os.path.exists(dst):
         shutil.rmtree(dst)
     os.mkdir(dst)
-    build.utils.copytree(src, dst)
+    pybuild.utils.copytree(src, dst)
 
-@build.task(['lib'])
+@pybuild.task(['lib'])
 def app(ctx):
-    build.cpp.compile(cf('{topdir}/example/src/app.cc'))
-    build.cpp.link_exe(
+    pybuild.cpp.compile(cf('{topdir}/example/src/app.cc'))
+    pybuild.cpp.link_exe(
        files=[cf('{build_dir}/{profile}/obj/app.o')],
        output='app',
        libs=['example']
     )
 
-@build.task(['install_headers'])
+@pybuild.task(['install_headers'])
 def lib(ctx):
-    build.cpp.compile(cf('{topdir}/example/src/lib.cc'))
-    build.cpp.create_static_lib(
+    pybuild.cpp.compile(cf('{topdir}/example/src/lib.cc'))
+    pybuild.cpp.create_static_lib(
         files=[cf('{build_dir}/{profile}/obj/lib.o')],
         output='libexample.a'
     )
 
-@build.task()
+@pybuild.task()
 def clean(ctx):
     shutil.rmtree(cf('{build_dir}/{profile}'))
 
-@build.task()
+@pybuild.task()
 def cppcheck(ctx):
-    build.run_cmd([
+    pybuild.run_cmd([
         'cppcheck',
         '--enable=all',
         '--error-exitcode=1',
@@ -46,13 +46,13 @@ def cppcheck(ctx):
         cf('{topdir}/example/src')
     ], print_stdout=True, print_stderr=True)
 
-@build.task()
+@pybuild.task()
 def test(ctx):
-    build.run_cmd(
+    pybuild.run_cmd(
         [cf('{build_dir}/{profile}/bin/app'), 'pybuild'],
         exit_on_fail=True,
         print_stderr=True,
         print_stdout=True
     )
 
-build.cli.run('app')
+pybuild.cli.run('app')
